4d1d699f035b69842a14cfd2471d3a11e2d67a48
Yaroslav Kuzmin <yarik1448kuzmin@gmail.com>
2024-03-26 23:45:55 +0300

    NOTES-1: default in schema fix

--- cmd/api/init/db/postgresql/schema/01_init.sql
+++ cmd/api/init/db/postgresql/schema/01_init.sql
@@ -2,6 +2,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE Notes (
     id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    title       VARCHAR(64),
-    plain_text  TEXT
+    title       VARCHAR(64) DEFAULT '',
+    plain_text  TEXT DEFAULT ''
 );
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/yarikTri/archipelago-notes-api
 
-go 1.20
+go 1.18
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
