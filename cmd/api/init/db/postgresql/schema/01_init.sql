85affb2f0dacdf34aff8f94e1795f3c513ec19ab
Yaroslav Kuzmin <yarik1448kuzmin@gmail.com>
2024-04-10 02:32:49 +0300

    lil typo fix

--- cmd/api/init/db/postgresql/schema/01_init.sql
+++ cmd/api/init/db/postgresql/schema/01_init.sql
@@ -3,5 +3,5 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE Notes (
     id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     automerge_url   VARCHAR(128) NOT NULL,
-    title           VARCHAR(64) DEFAULT '',
+    title           VARCHAR(64) DEFAULT ''
 );
