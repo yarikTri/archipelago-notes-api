3f05a33e2ba365b6888138b10f11d7890013ef60
stepan popov <s.d.popov@vk.team>
2025-03-28 00:18:30 +0300

    schema

---
+++ cmd/common/init/db/postgresql/schema/0003_tags.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tag (
+    tag_id UUID PRIMARY KEY,
+    name TEXT PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS tag_to_note (
+    tag_id UUID,
+    note_id UUID,
+    UNIQUE (tag_id, note_id)
+);
+
+CREATE TABLE IF NOT EXISTS tag_to_tag (
+    tag_1_id UUID,
+    tag_2_id UUID,
+    UNIQUE (tag_1_id, tag_2_id)
+);
