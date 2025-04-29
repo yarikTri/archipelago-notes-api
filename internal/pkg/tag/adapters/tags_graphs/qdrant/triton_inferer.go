6e54426f3041b636b1fc13d83076130df0829ae7
stepanpopov <stepan@DESKTOP-81FUGHF.localdomain>
2025-04-29 18:46:02 +0300

    triton inferer: fixed response struct

--- internal/pkg/tag/adapters/tags_graphs/qdrant/triton_inferer.go
+++ internal/pkg/tag/adapters/tags_graphs/qdrant/triton_inferer.go
@@ -3,6 +3,7 @@ package qdrant
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -27,8 +28,37 @@ type inferRequestInput struct {
 	Data     []string `json:"data"`
 }
 
-type inferResponse struct {
-	Vector []float32 `json:"vector"`
+type inferenceResponse struct {
+	ModelName    string                    `json:"model_name"`
+	ModelVersion string                    `json:"model_version"`
+	Parameters   map[string]interface{}    `json:"parameters"`
+	Outputs      []inferenceResponseOutput `json:"outputs"`
+}
+
+var VectorInResponseNotFound = errors.New("response contains zero outputs")
+
+func (r *inferenceResponse) getFirstVector() ([]float32, error) {
+
+	if len(r.Outputs) == 0 {
+		return nil, VectorInResponseNotFound
+	}
+
+	return r.Outputs[0].Data, nil
+}
+
+// Can be replaced for Parameters in InferenceResponse
+// .
+// type Parameters struct {
+//     SequenceID    int  `json:"sequence_id"`
+//     SequenceStart bool `json:"sequence_start"`
+//     SequenceEnd   bool `json:"sequence_end"`
+// }
+
+type inferenceResponseOutput struct {
+	Name     string    `json:"name"`
+	Datatype string    `json:"datatype"`
+	Shape    []int     `json:"shape"`
+	Data     []float32 `json:"data"`
 }
 
 func (i *TritonInferer) Infer(content string) ([]float32, error) {
@@ -48,6 +78,7 @@ func (i *TritonInferer) Infer(content string) ([]float32, error) {
 		return nil, err
 	}
 
+	// TODO: make port and host custom.
 	httpResp, err := http.Post(
 		"http://localhost:1234/v2/models/ensemble_model/infer",
 		"application/json",
@@ -67,10 +98,10 @@ func (i *TritonInferer) Infer(content string) ([]float32, error) {
 		return nil, err
 	}
 
-	var resp inferResponse
+	var resp inferenceResponse
 	if err := json.Unmarshal(body, &resp); err != nil {
 		return nil, err
 	}
 
-	return resp.Vector, nil
+	return resp.getFirstVector()
 }
